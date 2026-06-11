0c45220327d97b27f82d0006340422ea0a5736b2
Ayelet Hashahar Cohen <ahashahar@gmail.com>
2026-06-11 13:05:36 +0300

    Add expolration on the dataset

--- src/01_load_data.py
+++ src/01_load_data.py
@@ -26,7 +26,34 @@ def main():
 
     print(f"Saved {len(df)} rows to data/arxiv-summarization.csv")
 
+def load_data():
+    df = pd.read_csv("data/arxiv-summarization.csv")
+    return df
+
+def dataset_explore(df):
+    print(df.head())
+    print(df.columns)
+    print(df.shape)
+
+    print(len(df["article"][0]))
+    print(len(df["abstract"][0]))
+
+    print(len(df["article"][10]))
+    print(len(df["abstract"][10]))
+
+    print(len(df["article"][100]))
+    print(len(df["abstract"][100]))
+
+    print(f"Average length of article: {df['article'].apply(len).mean()}")
+    print(f"Average length of abstract: {df['abstract'].apply(len).mean()}")
+    print(f"Max length of article: {df['article'].apply(len).max()}")
+    print(f"Max length of abstract: {df['abstract'].apply(len).max()}")
+    print(f"Min length of article: {df['article'].apply(len).min()}")
+    print(f"Min length of abstract: {df['abstract'].apply(len).min()}")
+
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    df = load_data()
+    dataset_explore(df)
